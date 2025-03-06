861344c00265b7ee2ef61457dda00838f40e81d8
gustavogrijalba <34608825+gustavogrijalba@users.noreply.github.com>
2025-03-06 16:25:31 -0500

    Initial Commit

---
+++ StatsforSpotify/StatsforSpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatsforSpotifyApp.swift
+//  StatsforSpotify
+//
+//  Created by Gustavo  Grijalba on 3/6/25.
+//
+
+import SwiftUI
+
+@main
+struct StatsforSpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StatsforSpotifyTests/StatsforSpotifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  StatsforSpotifyTests.swift
+//  StatsforSpotifyTests
+//
+//  Created by Gustavo  Grijalba on 3/6/25.
+//
+
+import Testing
+@testable import StatsforSpotify
+
+struct StatsforSpotifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
